ead5b78082363f43667250f3fe09aefd27c0e43d
Adam Christopher Backlas Widjaja <adam.keristoper@gmail.com>
2017-05-22 11:50:51 +0900

    Implement deleting tickets feature

---
+++ spec/features/deleting_tickets_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Users can delete exisiting tickets" do
+  let(:project) {FactoryGirl.create(:project)}
+  let(:ticket) {FactoryGirl.create(:ticket, project: project)}
+
+  scenario "Deleting ticket successfully" do
+    visit project_ticket_path(project, ticket)
+    click_link "Delete Ticket"
+
+    expect(page).to have_content "Ticket has been deleted."
+    expect(page.current_url).to eq project_url(project)
+  end
+end
